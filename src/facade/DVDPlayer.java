5f4a08df03d5d05040299a2c1a880ca48ce52bc7
dana061113 <danakobylandykyzy@gmail.com>
2026-04-05 20:21:38 +0500

    Add DVDPlayer class with play, pause, and stop methods

---
+++ src/facade/DVDPlayer.java
@@ -0,0 +1,15 @@
+package facade;
+
+public class DVDPlayer {
+    public void play() {
+        System.out.println("DVD ойнап жатыр");
+    }
+
+    public void pause() {
+        System.out.println("DVD пауза");
+    }
+
+    public void stop() {
+        System.out.println("DVD тоқтады");
+    }
+}
